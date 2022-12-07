206df944ed1357a2a690955dfe930dfdb0f321c6
Bruno Brizolara <bbrizolara7@gmail.com>
2022-12-07 15:54:15 -0300

    Add persistence & refactor code

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,7 +11,7 @@ import {
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
-import { Task } from './tasks.model';
+import { Task } from './tasks.entity';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -19,37 +19,37 @@ export class TasksController {
   constructor(private taskService: TasksService) {}
 
   @Get()
-  getAllTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+  async getAllTasks(@Query() filterDto: GetTasksFilterDto): Promise<Task[]> {
     const result = this.taskService.getAllTasks(filterDto);
 
     return result;
   }
 
   @Get('/:id')
-  getTaskById(@Param('id') id: string): Task {
-    const task = this.taskService.getTaskById(id);
+  async getTaskById(@Param('id') id: string): Promise<Task> {
+    const task = await this.taskService.getTaskById(id);
 
     return task;
   }
 
   @Post()
-  createTask(@Body() { title, description }: CreateTaskDto): Task {
-    const task = this.taskService.createTask(title, description);
+  async createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
+    const task = await this.taskService.createTask(createTaskDto);
 
     return task;
   }
 
   @Delete('/:id')
-  deleteTask(@Param('id') id: string): void {
-    this.taskService.deleteTask(id);
+  async deleteTask(@Param('id') id: string): Promise<void> {
+    await this.taskService.deleteTask(id);
   }
 
   @Patch('/:id/status')
-  updateTaskStatus(
+  async updateTaskStatus(
     @Param('id') id: string,
     @Body() { status }: UpdateTaskStatusDto,
-  ): Task {
-    const task = this.taskService.updateTaskStatus(id, status);
+  ): Promise<Task> {
+    const task = await this.taskService.updateTaskStatus(id, status);
 
     return task;
   }
