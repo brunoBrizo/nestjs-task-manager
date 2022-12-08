2ae5592f417b73e5a2244d83c0bc7cf92acdff50
Bruno Brizolara <bbrizolara7@gmail.com>
2022-12-08 13:44:28 -0300

    Add loggin & handling task errors (#5)

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { TransformInterceptor } from './transform.interceptor';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('Main App');
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
+
   await app.listen(3000);
+  logger.log(`App running on port 3000`);
 }
 bootstrap();
