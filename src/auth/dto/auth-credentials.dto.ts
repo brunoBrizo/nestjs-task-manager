aae25dc0b16737007c6c2718c77a6575b907c3bd
Bruno Brizolara <bbrizolara7@gmail.com>
2022-12-08 11:35:09 -0300

    Add authentication & authorization

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  userName: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password must have upper and lower case letters, at least 1 number o special character',
+  })
+  password: string;
+}
